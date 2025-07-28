316d28cd3ddc6011c960a82630064317f9d9515d
ArvinderPal10 <arvinderpal10@gmail.com>
2025-07-28 20:23:21 +0530

    Updated git commands

--- gitbasics.py
+++ gitbasics.py
@@ -18,3 +18,14 @@
 # Untracked -- That means it is a newly created file not yet available on Github
 
 # Modified -- The file already exists but the content is updated 
+
+
+# git status 
+
+# git add filename or git add . -- to add all the files in the current folder
+
+# git commit -m "your message"
+
+# git push 
+
+# git pull
