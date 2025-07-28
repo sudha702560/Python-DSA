e0c94ff7fc9ecc801e5e4b9aaf46441cc8cef6ab
ArvinderPal10 <arvinderpal10@gmail.com>
2025-07-28 21:32:28 +0530

    Questions based on Pattern 1

---
+++ 0001.Pattern1-Digits/Practice/Assignment.py
@@ -0,0 +1,31 @@
+# -----------------------------------
+# Pattern 1 - Getting the Digit (// and %)
+# -----------------------------------
+
+# Find count of digits of a number 
+# Sum of digits of a number 
+# Find the prodcut of all the digits of a number 
+# Count of Digits greater than a specific number 
+# Find the largest Digit in a number 
+# Count the freq of a specific digit
+# Count the factors of a number 
+# Count the composite and Prime Numbers 
+# COunt the freq of each and every digit
+# Average of sum of digits 
+# Count multiples of a number
+# Sum of Even and Odd digits 
+
+
+
+# -----------------------------------
+# Leetcode Questions 
+# -----------------------------------
+
+# 2544. Alternating Digit Sum
+# 2553. Separate the Digits in an Array
+# 1295. Find Numbers with Even Number of Digits       
+# 2520. Count the Digits That Divide a Number
+# 1281. Subtract the Product and Sum of Digits of an Integer
+# 2180. Count Integers With Even Digit Sum   
+# 1796. Second Largest Digit in a String
+# 258. Add Digits
