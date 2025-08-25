cc35f6c051fcc2f2dfa8bbfaac1d80885ee43b51
ArvinderPal10 <arvinderpal10@gmail.com>
2025-08-25 21:09:27 +0530

    Leetcode 88 Python

---
+++ Arrays/swap.py
@@ -0,0 +1,17 @@
+a = 120
+b = 100
+# print(a,b)
+# a , b  = b, a
+# print(a,b)
+
+# using third variable 
+# print(a,b)
+# temp = a 
+# a = b 
+# b = temp 
+# print(a,b)
+print(a,b)
+b = a+b    # 220 
+a = b -a    # 100
+b = b - a    # 120 
+print(a,b)
